dac60852487dda328ac01339635c871b4b17ffb8
selyn woo <wooddfly@gmail.com>
2023-07-18 15:33:04 +0900

    테스트

--- app.js
+++ app.js
@@ -3,54 +3,26 @@ require('dotenv').config();
 
 // DEPENDENCIES
 const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-
-// SSR
-const { createBundleRenderer } = require("vue-server-renderer");
-const services = require("./server/services");
-
 
 const app = express();
 const port = process.env.PORT || 4000;
-const cors = require('cors');
 
-//app.use(express.json());
-app.use(express.static('./public')); //Static File service
-app.use(cors());
-// Body-parser
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
-// Node의 native Promise 사용
-mongoose.Promise = global.Promise;
-mongoose.set("strictQuery", false);
+app.use(express.static('./public')); 
 
-// Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-	useUnifiedTopology: true,
-	useNewUrlParser: true,
-}).then(() => console.log('Successfully connected to mongodb')).catch(e => console.error(e));
-
-// ROUTERS
 app.all('/*', async ({ url}, res, next) => {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "X-Requested-With");
-	if (services.checkApi(url)) {
-		next();
-		return;
-	}
-	if (services.getSSRPage(url) !== -1) {
-		//res.send(await ssrService.getRenderedHtml("ssr", { url }));
-		return res.send(await services.renderByServer({ url }));
-	} else {
-		return res.send(await services.renderByClient({ url }));
-	}
+	next();
 });
 
-app.use('/api/project', require('./router/project'));
+app.get('/', (req, res) => {
+	res.render('index')
+});
 
 app.listen(port, () => {
-	console.log(`Server listening on port number ${port}`);
+	console.log(`Server listening on port ${port}`);
 	console.log(`listen to http://localhost:${port}`);
 });
