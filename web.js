7abafdb1724f52116c83cee75dab0ff360bc6267
selyn woo <wooddfly@gmail.com>
2023-07-18 15:17:20 +0900

    테스트

--- web.js
+++ web.js
@@ -1,12 +1,25 @@
-const express = require('express')
-const app = express()
-const PORT = 8001
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs')
+// ENV
+require('dotenv').config();
+
+// DEPENDENCIES
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+//app.use(express.static('./public')); 
+
+app.all('/*', async ({ url}, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	next();
+});
+
 app.get('/', (req, res) => {
 	res.render('index')
-})
-app.listen(PORT, () => {
-	console.log(`server started on PORT ${PORT}`)
-})
-// web_bk.js
+});
+
+app.listen(port, () => {
+	console.log(`Server listening on port ${port}`);
+	console.log(`listen to http://localhost:${port}`);
+});
