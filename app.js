40b5cb22994936b1a0413f9bf929fb3e6e23ba59
selyn woo <wooddfly@gmail.com>
2023-07-18 15:48:18 +0900

    테스트

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 
 // DEPENDENCIES
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -10,7 +11,8 @@ const port = process.env.PORT || 4000;
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.use(express.static('./public')); 
+//app.use(express.static('./public'));
+app.use(express.static(path.join(__dirname, "public")));
 //app.use('/public', express.static( __dirname + '/public'));
 
 app.all('/*', async ({ url}, res, next) => {
